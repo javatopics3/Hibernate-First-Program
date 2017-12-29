9d24aa397cc1da54674e0241a28dd5cddc4d8a99
Raghunadh <Raghunadh@RAGHU>
2017-12-29 10:36:51 +0530

    2nd commit

--- src/com/app/Test.java
+++ src/com/app/Test.java
@@ -15,7 +15,7 @@ public class Test {
 		Transaction tx = ses.beginTransaction();
 		tx.begin();
 		Student stu = new Student();
-		stu.setStdId(910);
+		stu.setStdId(9155);
 		stu.setStdName("AAAbbbb");
 		stu.setStdFee(54533.42);
 		ses.save(stu);
